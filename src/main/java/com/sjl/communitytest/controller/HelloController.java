95490b4959ee8992d583505d698289a23a40e7b5
Administrator <467396631@qq.com>
2019-08-04 16:02:25 +0800

    import bootStrap and Github OAuth

--- src/main/java/com/sjl/communitytest/controller/HelloController.java
+++ src/main/java/com/sjl/communitytest/controller/HelloController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class HelloController
 {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String hello(@RequestParam(name = "name", required = false) String name, Model model)
     {
         model.addAttribute("name", name);
 
-        return "hello";
+        return "index";
     }
 }
