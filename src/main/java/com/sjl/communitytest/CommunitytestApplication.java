896d52f03f1d9ffdcec1541005c36264414b661b
Administrator <467396631@qq.com>
2019-08-04 11:09:36 +0800

    Create CommunityTest

---
+++ src/main/java/com/sjl/communitytest/CommunitytestApplication.java
@@ -0,0 +1,15 @@
+package com.sjl.communitytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**@author
+ */
+@SpringBootApplication
+public class CommunitytestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitytestApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sjl/communitytest/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.sjl.communitytest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @description:
+ * @author: Mr.S
+ * @create: 2019-08-04 09:57
+ **/
+@Controller
+public class HelloController
+{
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name", required = false) String name, Model model)
+    {
+        model.addAttribute("name", name);
+
+        return "hello";
+    }
+}
